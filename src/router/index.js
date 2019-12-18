c9b5600cd7dacb16a56481d03285f50c97e646cf
Xanonymous <trusaidlin@gmail.com>
2019-12-19 02:45:03 +0800

    add nav-drawer and toolbar but need to fix layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import MainPage from "@/views/MainPage";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    name: "mainpage",
+    component: MainPage
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/test",
+    name: "test",
+    component: () => import("@/components/NavigationDrawer")
   }
 ];
 
---
+++ src/views/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <ToolBar />
+    <NavigationDrawer />
+  </v-app>
+</template>
+
+<script>
+import ToolBar from "@/components/ToolBar";
+import NavigationDrawer from "@/components/NavigationDrawer";
+export default {
+  name: "mainpage",
+  components: {
+    ToolBar,
+    NavigationDrawer
+  }
+};
+</script>
