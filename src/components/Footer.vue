aac2479a7c1a7e798b331d06fb2f5007ac31264c
TeU-Lin <trusaidlin@gmail.com>
2019-12-25 01:36:54 +0800

    init apis module (#4)
    * init apis module
    
    * components seperated
    
    * bar menu

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-footer app color="deep-purple accent-4">
+    <span class="white--text">Copyright &copy; NPC GO Dev Team</span>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "footer"
+};
+</script>
