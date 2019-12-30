7634d9141d0694207463b715836bec16fa0de09a
Xanonymous <trusaidlin@gmail.com>
2019-12-31 04:19:50 +0800

    Settings components mounted bug fixed

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -5,7 +5,21 @@
 <script>
 import { mapGetters } from "vuex";
 export default {
-  name: "settings"
+  name: "settings",
+  mounted() {
+    this.$store.dispatch("setPageDynamicConfig", {
+      name: "currentPage",
+      data: this.$router.currentRoute.name
+    });
+    this.$store.dispatch("setPageDynamicConfig", {
+      name: "selectedItemInCurrentPage",
+      data: []
+    });
+    this.$store.dispatch("setPageDynamicConfig", {
+      name: "selectionMode",
+      data: false
+    });
+  }
 };
 </script>
 
