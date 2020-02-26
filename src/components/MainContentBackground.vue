8fe1187c3960ea9210866092b3918b8df9fbf455
li wei <wayne900619@gmail.com>
2020-02-26 13:42:03 +0800

    change background img

--- src/components/MainContentBackground.vue
+++ src/components/MainContentBackground.vue
@@ -3,7 +3,7 @@
     <v-row align="center" justify="center">
       <v-col align="center" justify="center">
         <v-img
-          src="https://education.github.com/assets/sdp-backpack-a64038716bf134f45e809ff86b9611fb97e41bbd2ccfa3181da73cf164d3c200.png"
+          src="@/assets/majaja.png"
           aspect-ratio="0.0001"
           max-width="229"
           max-height="264"
